9f4624d8142997c4dbf1c3d2c37500dd22f9c137
Sean <a1668979@student.adelaide.edu.au>
2017-12-06 23:12:25 +0000

    Created a copy function

--- files/filewriter.go
+++ files/filewriter.go
@@ -1,1 +1,23 @@
-package io
+package files
+
+import (
+	"io"
+	"os"
+)
+
+// Copy will read the contents of the path defined by src
+// and output the content to dst
+func Copy(src, dst string) error {
+	from, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+	to, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+	_, err = io.Copy(to, from)
+	return err
+}
